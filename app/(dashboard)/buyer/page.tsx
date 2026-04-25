6dcbfb7367b9dbd6b1ea979d6a4e06d275babb33
Megabyte-webdev <afolabimubarak18@gmail.com>
2026-04-25 01:18:51 +0100

    latest

--- app/(dashboard)/buyer/page.tsx
+++ app/(dashboard)/buyer/page.tsx
@@ -1,9 +1,9 @@
-// const page = () => {
-//   return (
-//     <main className="w-full h-full mx-auto flex p-4 md:p-8 md:pt-4.75 overflow-y-auto bg-white">
-//       Home
-//     </main>
-//   );
-// };
+const page = () => {
+  return (
+    <main className="w-full h-full mx-auto flex p-4 md:p-8 md:pt-4.75 overflow-y-auto bg-white">
+      Home
+    </main>
+  );
+};
 
-// export default page;
+export default page;
