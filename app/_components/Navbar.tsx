d14f4c4ee5fb38f0ebcdaa34569b6bd0bc6fc78f
Megabyte-webdev <afolabimubarak18@gmail.com>
2026-05-06 00:34:42 +0100

    fix the main

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -58,19 +58,6 @@ const Navbar = () => {
             />
           </div>
 
-          {/* Desktop Links */}
-          <div className="ml-auto mr-2 hidden md:flex items-center gap-6 text-gray-700 text-[13px] font-medium">
-            {navLinks.map((link: { name: string; path: string }) => (
-              <Link
-                key={link.name}
-                href={link.path}
-                className="hover:text-black cursor-pointer transition-colors"
-              >
-                {link.name}
-              </Link>
-            ))}
-          </div>
-
           {/* Right Actions */}
           <div className="flex items-center gap-3">
             {!user && (
@@ -84,7 +71,7 @@ const Navbar = () => {
             )}
 
             {/* Mobile Toggle */}
-            <button
+            {/* <button
               onClick={() => setIsOpen(!isOpen)}
               className="md:hidden flex items-center justify-center w-8.5 h-8.5 rounded-lg border-2 border-black bg-white transition-all active:scale-90"
             >
@@ -93,7 +80,7 @@ const Navbar = () => {
               ) : (
                 <Menu size={18} strokeWidth={2.5} />
               )}
-            </button>
+            </button> */}
           </div>
 
           {isOpen && (
