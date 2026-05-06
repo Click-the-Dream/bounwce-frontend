7832e0af927705802298b042ccc3f7e843c144bd
Megabyte-webdev <afolabimubarak18@gmail.com>
2026-05-07 00:11:34 +0100

    latest

--- app/(store)/stores/_components/StoreFront.tsx
+++ app/(store)/stores/_components/StoreFront.tsx
@@ -130,7 +130,7 @@ const StoreFront = () => {
                 </div>
               ) : (
                 <>
-                  <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                  <div className="grid md:grid-cols-auto lg:grid-cols-4 gap-6">
                     {productList.map((product: any) => (
                       <ProductCard key={product.id} product={product} />
                     ))}
