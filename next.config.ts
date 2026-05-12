2ad5b8a2620c7731ad557f2418ac2cba7d16b0f3
Click-the-Dream <justclick610@gmail.com>
2026-05-13 00:20:03 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,19 +9,19 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "images.pexels.com", pathname: "/**" },
     ],
   },
-  async rewrites() {
-    // ONLY apply the proxy if we are running locally
-    if (process.env.NODE_ENV === 'development') {
-      return [
-        {
-          source: "/proxy-api/:path*",
-          destination: "https://click-backend-j7yi.onrender.com/api/v1/:path*",
-        },
-      ];
-    }
-    // Return an empty array in production
-    return [];
-  },
+  // async rewrites() {
+  //   // ONLY apply the proxy if we are running locally
+  //   if (process.env.NODE_ENV === 'development') {
+  //     return [
+  //       {
+  //         source: "/proxy-api/:path*",
+  //         destination: "https://click-backend-j7yi.onrender.com/api/v1/:path*",
+  //       },
+  //     ];
+  //   }
+  //   // Return an empty array in production
+  //   return [];
+  // },
 };
 
 export default nextConfig;
