94a25d3a3670397a3b4206793a22a6d875184884
Click-the-Dream <justclick610@gmail.com>
2026-05-13 01:09:55 +0100

    Update api.ts

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/proxy-api`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
   timeout: 60000,
   withCredentials: true, // REQUIRED for refresh cookies
 });
