6825e19136a3b3d820f237bc07159661df906361
Click-the-Dream <justclick610@gmail.com>
2026-06-20 22:47:45 +0100

    Update IdentityCard.tsx

--- app/(dashboard)/app/profile/_components/IdentityCard.tsx
+++ app/(dashboard)/app/profile/_components/IdentityCard.tsx
@@ -213,7 +213,7 @@ const IdentityCard: React.FC<Props> = ({
             {data.name}
           </h2>
 
-          <p className="text-[#888888] text-[13px]">@{data.handle}</p>
+          <p className="text-[#888888] text-[13px] line-clamp-2">@{data.handle}</p>
         </div>
 
         {isOwnProfile && (
