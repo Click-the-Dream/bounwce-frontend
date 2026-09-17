65e0742173993018ebb73403ec71c239037f9436
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2026-09-17 03:17:43 +0100

    Update useGeneratedHtml.ts

--- app/hooks/useGeneratedHtml.ts
+++ app/hooks/useGeneratedHtml.ts
@@ -37,7 +37,7 @@ const useGeneratedHtml = (formData: any) => {
       font-size: 15px; 
       color: #374151; 
       line-height: 1.8; 
-      
+      overflow-wrap: break-word !important;
        
       
     }
