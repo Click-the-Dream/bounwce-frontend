3158aed5577812c9c0c39c1579d4d98e59846c8b
Afolabi Mubarak <126519914+Megabyte-webdev@users.noreply.github.com>
2026-09-17 03:46:48 +0100

    Update EditorPreview.tsx

--- app/(dashboard)/admin/newsletter/_components/editor/EditorPreview.tsx
+++ app/(dashboard)/admin/newsletter/_components/editor/EditorPreview.tsx
@@ -1,15 +1,18 @@
 const EditorPreview = ({ previewMode, generatedHtml }: any) => {
   return (
     <main
-      className={`flex-1 p-4 md:p-10 flex justify-center items-center ${
+      className={`flex-1 p-2 md:p-10 flex justify-center items-center overflow-hidden ${
         previewMode === "desktop" ? "hidden md:flex" : "flex"
       }`}
     >
-      <iframe
-        title="Preview"
-        srcDoc={generatedHtml}
-        className="w-full h-full border-none"
-      />
+      <div className="w-full h-full max-w-[650px] mx-auto overflow-hidden rounded-lg shadow-sm">
+        <iframe
+          title="Preview"
+          srcDoc={generatedHtml}
+          className="w-full h-full border-none block"
+          style={{ width: "100%", height: "100%" }}
+        />
+      </div>
     </main>
   );
 };
