327df143b70dd047c162be610b7528d8ba89aa18
Click-the-Dream <justclick610@gmail.com>
2026-09-23 16:53:50 +0100

    Update UserImage.tsx

--- app/(dashboard)/app/_components/UserImage.tsx
+++ app/(dashboard)/app/_components/UserImage.tsx
@@ -169,7 +169,7 @@ const UserImage = ({
                   {isCurrentProfilePage ? (
                     <button
                       onClick={handleShare}
-                      className="flex-1 inline-flex items-center justify-center gap-2 rounded-2xl bg-emerald-500 px-4 py-3 text-sm font-semibold text-white shadow-xs transition hover:bg-emerald-600 active:scale-[0.98]"
+                      className="flex-1 inline-flex items-center justify-center gap-2 rounded-2xl bg-black px-4 py-3 text-sm font-semibold text-white shadow-xs transition hover:bg-black/90 active:scale-[0.98]"
                     >
                       <Share2 size={18} />
                       Share Profile
